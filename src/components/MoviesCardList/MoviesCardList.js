c022572336efaa52bed2c1f9104a3d10ae2744d1
Florn67 <edgewalker909@gmail.com>
2022-03-10 21:36:39 +0300

    feat: movie card

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,9 +1,17 @@
 import "./MoviesCardList.css";
-import Film from "../Film/Film";
+import MoviesCard from "../MoviesCard/MoviesCard";
 function MoviesCardList() {
+  let grid = [];
+  for(let i =0; i < 10; i+=1){
+    grid.push(<MoviesCard></MoviesCard>)
+  }
   return (
     <section className="movies-card-list">
-        <Film></Film>
+      <div className="movies-card-list__movies-container">
+        {grid}
+        <MoviesCard></MoviesCard>
+      </div>
+      <button className="movies-card-list__button">Ещё</button>
     </section>
   );
 }
